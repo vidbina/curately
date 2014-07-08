3011212e558697438da734e78ee0860ca3548cde
David Asabina <david@supr.nu>
2014-07-08 15:18:07 +0200

    Initial Devise setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140517145428) do
+ActiveRecord::Schema.define(version: 20140708125913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
+
+  create_table "accountants", id: :uuid, default: "uuid_generate_v4()", force: true do |t|
+    t.string   "name",       null: false
+    t.string   "shortname",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "accountants", ["shortname"], name: "index_accountants_on_shortname", unique: true, using: :btree
 
   create_table "clients", id: :uuid, default: "uuid_generate_v4()", force: true do |t|
     t.string   "name",       null: false
@@ -26,5 +35,23 @@ ActiveRecord::Schema.define(version: 20140517145428) do
   end
 
   add_index "clients", ["shortname"], name: "index_clients_on_shortname", unique: true, using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
