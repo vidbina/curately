3f1ba91e33b7f7fa9c249cd55670c372e43466fd
David Asabina <david@supr.nu>
2014-07-08 15:47:52 +0200

    Testing user creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) { {
+    email: 'yoda@jedi.org',
+    password: 'Entry, I request'
+  } }
+
+  it "It is saved is valid" do
+    expect {
+      User.create(valid_attributes).should be_persisted
+    }.to change(User, :count).by(1)
+  end
 end
