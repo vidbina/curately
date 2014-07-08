259ad51bda5c9ff67b71ed877fd09a47bae6c112
David Asabina <david@supr.nu>
2014-07-08 23:44:00 +0200

    POSPONED: request specs... will do that in near future

--- spec/requests/accountants_spec.rb
+++ spec/requests/accountants_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe "Accountants" do
-  describe "GET /accountants" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get accountants_path
-      response.status.should be(200)
-    end
-  end
+  let(:user) {
+    User.create(email: 'yoda@jedi.org', password: 'Entry, I request')
+  }
+
+#  describe "GET /accountants" do
+#    it "returns all accountants when logged in" do
+#      sign_in :user, user
+#      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#      get accountants_path
+#      response.status.should be(200)
+#    end
+#  end
 end
