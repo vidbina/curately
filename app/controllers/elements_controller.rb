15a635151024b510b9abf23f1abb81d0c5fdd2da
David Asabina <david@supr.nu>
2014-07-22 08:22:58 +0200

    Updated spec for elements controller to specify actual wanted behavior

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,22 +1,73 @@
 class ElementsController < ApplicationController
+  #before_action :authenticate_user!
+  before_action :set_element #, only: [:show, :edit, :update, :destroy]
+  before_action :testthis
+
+  load_and_authorize_resource :curator
+  load_and_authorize_resource :template
+  load_resource :element
+
   def index
+    p "INDEX" #{@template
+    @elements = [] # @template.elements
   end
 
   def show
   end
 
   def new
+    @element = Element.new
   end
 
   def edit
   end
 
   def create
+    @element = Element.new(element_params)
+
+    respond_to do |format|
+      if @element.save
+        format.html { redirect_to @element, notice: 'Element was successfully created.' }
+        format.json { render :show, status: :created, location: @element }
+      else
+        format.html { render :new }
+        format.json { render json: @element.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @element.update(element_params)
+        format.html { redirect_to @element, notice: 'Element was successfully updated.' }
+        format.json { render :show, status: :ok, location: @element }
+      else
+        format.html { render :edit }
+        format.json { render json: @element.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @element.destroy
+  end
+
+  private
+  def set_element
+    #p "setting element"
+    @curator = Curator.find(params[:curator_id])
+    @template = @curator.template
+    p "template is #{@template}"
+    @element  = @template.elements.find(:id)
+    p "element is #{@element}"
+  end
+
+  def testthis
+    p "testing"
+  end
+
+  def element_params
+    p({ curator_id: params[:curator_id] }.merge params.require(:element).permit(:name))
+    { curator_id: params[:curator_id] }.merge params.require(:element).permit(:name)
   end
 end
--- spec/controllers/elements_controller_spec.rb
+++ spec/controllers/elements_controller_spec.rb
@@ -12,24 +12,26 @@ describe ElementsController, :type => :controller do
   end
 
   describe "GET index" do
-    it "returns http success" do
+    it "returns all elements for the template" do
+      2.times { create(:element, template: curatorship.curator.template) }
       get :index, request_params
-      expect(response).to be_success
+      expect(assigns(:elements)).to match_array(curatorship.curator.template.elements)
     end
   end
 
   describe "GET show" do
-    it "returns http success" do
+    it "shows the element details" do
       request_params[:id] = element.id
       get :show, request_params
-      expect(response).to be_success
+      expect(assigns(:element)).to eq(element)
     end
   end
 
   describe "GET new" do
-    it "returns http success" do
+    it "prepared a new element" do
       get :new, request_params
-      expect(response).to be_success
+      p request.url
+      expect(assigns(:element)).to be_a_new(Element)
     end
   end
 
@@ -37,30 +39,38 @@ describe ElementsController, :type => :controller do
     it "returns http success" do
       request_params[:id] = element.id
       get :edit, request_params
-      expect(response).to be_success
+      expect(response).to have_http_status(:success)
+    end
+
+    it "offers the existing element for modification" do
+      request_params[:id] = element.id
+      get :edit, request_params
+      expect(assigns(:element)).to be_a_new(Element)
     end
   end
 
   describe "POST create" do
-    it "returns http success" do
+    it "creates the resource upon submission" do
+      request_params[:element] = build(:element).attributes
       post :create, request_params
-      expect(response).to be_success
+      expect(Element).to receive(:create)
     end
   end
 
   describe "PUT update" do
-    it "returns http success" do
+    it "performs the update" do
       request_params[:id] = element.id
+      request_params[:element] = build(:element).attributes
       put :update, request_params
-      expect(response).to be_success
+      expect_any_instance_of(Element).to receive(:update)
     end
   end
 
   describe "DELETE destroy" do
-    it "returns http success" do
+    it "removes the element" do
       request_params[:id] = element.id
       delete :destroy, request_params
-      expect(response).to be_success
+      expect_any_instance_of(Element).to receive(:destroy)
     end
   end
 
