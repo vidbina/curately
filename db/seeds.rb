d5ffc7c7afe880940d1bb385e6a431b0d6cedadf
David Asabina <david@supr.nu>
2014-08-01 07:47:47 +0200

    Added a few curators and members :)

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,8 @@ class Seed
     # journal of the leading cosmographic society in our galaxy, a major news
     # reporting channel and another cool magazine.
     spock      = create(:user, email: 'spock@enterprise.org', password: 'Live long')
-    facts      = create(:template, name: 'Intergallactic Facts')
+    kirk       = create(:user, email: 'kirk@enterprise.org', password: 'Still captain')
+    facts      = create(:template, name: 'Intergallactic Observations')
                  create(:element, name: 'average temperature', template: facts)
                  create(:element, name: 'average disposition', template: facts)
                  create(:element, name: 'airborne vessels', template: facts)
@@ -28,44 +29,58 @@ class Seed
                  create(:client,  name: 'Interstellar Cosmography', shortname: 'intcosmo', curator: enterprise)
                  create(:client,  name: 'Laser News Network', shortname: 'lnn', curator: enterprise)
                  create(:client,  name: 'The New New Yorker', shortname: 'newnewyorker', curator: enterprise)
+                 create(:curatorship, curator: enterprise, user: kirk, is_admin: true)
+                 create(:curatorship, curator: enterprise, user: spock, is_admin: false)
   end
     
   def build_force
     # Yoda shares his zen wisdown with all who are interested in mastering the
     # force.
     yoda       = create(:user, email: 'yoda@jedi.org', password: 'Entry, I seek')
+    zelda      = create(:user, email: 'zelda@hyrulebalance.org', password: 'Balance today')
+    jasmine    = create(:user, email: 'jasmine@jasnbob.usa', password: 'Bog makes awful veggie burgers')
+    bob        = create(:user, email: 'bob@jasnbob.usa', password: 'I make delightful veggie burgers')
+    kwame      = create(:user, email: 'kwame@jasnbob.usa', password: 'They always talk about stupid burgers')
     ratios     = create(:template, name: 'Balances')
                  create(:element, name: 'entrepreneurship', template: ratios)
                  create(:element, name: 'socialness', template: ratios)
                  create(:element, name: 'creativity', template: ratios)
     slowforce  = create(:curator, name: 'Slow Force', shortname: 'slowforce', template: ratios)
-                 create(:client,  name: 'Jasmine & Bob Yoga', shortname: 'jasmineyoga', curator: slowforce)
+    jas_n_bob  = create(:client,  name: 'Jasmine & Bob Yoga', shortname: 'jasmineyoga', curator: slowforce)
                  create(:client,  name: 'Musicology Ltd', shortname: 'musicology', curator: slowforce)
-                 create(:client,  name: 'New Mixter', shortname: 'newmixter', curator: slowforce)
+    hyrule     = create(:client,  name: 'Hyrule Balance', shortname: 'hyrulebalance', curator: slowforce)
+                 create(:membership, client: hyrule, user: zelda, is_admin: true)
+                 create(:membership, client: jas_n_bob, user: jasmine, is_admin: true)
+                 create(:membership, client: jas_n_bob, user: bob, is_admin: true)
+                 create(:membership, client: jas_n_bob, user: kwame, is_admin: false)
   end
     
   def build_books
     # Bookkeeper sharing data with its clients
     bookkeeper = create(:user, email: 'simone@chi.service', password: 'She rocks')
+    daughter   = create(:user, email: 'angela@chi.service', password: 'I rock too much')
     numbers    = create(:template, name: 'Results')
                  create(:element, name: 'revenue', template: numbers)
                  create(:element, name: 'expenses', template: numbers)
                  create(:element, name: 'profit', template: numbers)
                  create(:element, name: 'loss', template: numbers)
-    chicola    = create(:curator, name: 'Chi, Daughters & Co', shortname: 'chicoLA', template: numbers)
-                 create(:client, name: 'Sparrow Hair Studio', shortname: 'sparrowhair', curator: chicola)
-                 create(:client, name: 'Premium Tobacco', shortname: 'premiumsmokes', curator: chicola)
-                 create(:client, name: 'Bobby Burgers', shortname: 'bburgers', curator: chicola)
-                 create(:client, name: 'BlastYard Labs', shortname: 'blastlab', curator: chicola)
+    chi_and_co = create(:curator, name: 'Chi, Daughters & Co', shortname: 'chiandco', template: numbers)
+                 create(:client, name: 'Sparrow Hair Studio', shortname: 'sparrowhair', curator: chi_and_co)
+                 create(:client, name: 'Premium Tobacco', shortname: 'premiumsmokes', curator: chi_and_co)
+                 create(:client, name: 'Bobby Burgers', shortname: 'bburgers', curator: chi_and_co)
+                 create(:client, name: 'BlastYard Labs', shortname: 'blastlab', curator: chi_and_co)
+                 create(:curatorship, curator: chi_and_co, user: bookkeeper, is_admin: true)
+                 create(:curatorship, curator: chi_and_co, user: daughter, is_admin: false)
   end
 
   def build_law
     # Lawyer sharing advice with its clients
-    lawyer     = create(:user, email: 'd.a.nierstein@nierstein.law', password: 'password')
+    lawyer     = create(:user, email: 'd.a.nierstein@nierstein.law', password: 'password in latin is signum')
     lawstream  = create(:template, name: 'Updates')
                  create(:element, name: 'ammendments', template: lawstream)
     nierstein  = create(:curator, name: 'Nierstein Law', shortname: 'nierstein', template: lawstream)
                  create(:client, name: 'White Partner Laundromats', shortname: 'whitewash', curator: nierstein)
+                 create(:curatorship, curator: nierstein, user: lawyer, is_admin: true)
   end
 end
 
