e10af282574c8ec0ae24d0409657368f185f34ba
David Asabina <david@supr.nu>
2014-08-02 13:28:50 +0200

    Creating the element views and corresponding specs

--- spec/views/elements/edit.html.erb_spec.rb
+++ spec/views/elements/edit.html.erb_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "elements/edit.html.erb", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "elements/edit.html.erb", :type => :view do
+  before(:each) do
+    @curator = assign(:curator, stub_model(Curator, attributes_without_id(build(:curator))))
+    @template = assign(:template, @curator.template)
+    #assign(:element, @element = @template.elements.build(attributes_without_id(build(:element))))
+    @template.elements << (@element = assign(:element, stub_model(Element, attributes_without_id(build(:element)))))
+    #allow(@template).to receive(:elements).and_return([@element])
+    allow(@element).to receive(:new_record?).and_return(false)
+  end
+
+  it "renders the name input field in the form" do
+    render
+    assert_select "form[action=?][method=?]", curator_template_element_path(curator_id: @curator.id, id: @element.id), "post" do
+      assert_select "input#element_name[name=?]", "element[name]"
+    end
+  end
 end
--- spec/views/elements/show.html.erb_spec.rb
+++ spec/views/elements/show.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "elements/show.html.erb", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "elements/show.html.erb", :type => :view do
+  before(:each) do
+    assign(:curator, stub_model(Curator, attributes_without_id(build(:curator))))
+    @element = assign(:element, stub_model(Element, attributes_without_id(build(:element))))
+  end
+
+  it "displays the element's name" do
+    render
+    expect(rendered).to include(@element.name)
+  end
 end
